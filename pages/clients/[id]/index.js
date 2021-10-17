fd314dc4345cd9ef51295fb6b483224a35e866fd
Gimhana Dissanayake <46950713+Gimhana-Dissanayake@users.noreply.github.com>
2021-10-17 14:02:00 +0530

    63.navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -6,9 +6,18 @@ const ClientProjectsPage = () => {
 
   console.log(router.query);
 
+  function loadProjectHandler() {
+    //load data...
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 };
