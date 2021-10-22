60b3e61c4a95113b4a4390cfb3778465818e965f
Giimhana Dissanayake <giimhanadissanayake@gmail.com>
2021-10-22 10:57:00 +0530

    88.nextjs pre-renders by default

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,10 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h1>Hello Next World!</h1>
-    </div>
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
   );
 }
