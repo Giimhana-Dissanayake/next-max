9a7f91e8d11012028baae32b0320a70cba0bad61
Giimhana Dissanayake <giimhanadissanayake@gmail.com>
2021-10-22 21:56:40 +0530

    116.adding static site generation(SSG) on the home page

---
+++ helpers/api-util.js
@@ -0,0 +1,22 @@
+export async function getAllEvents() {
+  const response = await fetch(
+    "https://ninja-firestore-tut-ce3c6.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+  return allEvents.filter((event) => event.isFeatured);
+}
