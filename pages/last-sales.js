2df36fbeb7c0bf89924a3663c91d6e88dc9c7de9
Giimhana Dissanayake <giimhanadissanayake@gmail.com>
2021-10-22 20:07:30 +0530

    111.combining pre-fetching with client side fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,36 +1,54 @@
 import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 
-function LastSalesPage() {
-  const [state, setState] = useState();
+function LastSalesPage(props) {
+  const [state, setState] = useState(props.state);
 
-  const [isLoading, setIsLoading] = useState(false);
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    (url) => fetch(url).then((res) => res.json())
+  );
 
   useEffect(() => {
-    setIsLoading(true);
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        return res.json();
-      })
-      .then((json) => {
-        setState(json);
-        setIsLoading(false);
-      })
-      .catch(() => {});
-  }, []);
+    setState(data);
+  }, [data]);
 
-  console.log("state ", state);
+  //   useEffect(() => {
+  //     setIsLoading(true);
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((res) => {
+  //         return res.json();
+  //       })
+  //       .then((json) => {
+  //         setState(json);
+  //         setIsLoading(false);
+  //       })
+  //       .catch(() => {});
+  //   }, []);
 
-  if (isLoading) {
+  if (error) {
+    return <p>Failed to load</p>;
+  }
+
+  if (!data && !state) {
     return <p>Loading...</p>;
   }
 
-  if (!state) {
-    return <p>No data yet</p>;
-  }
+  return state ? (
+    state.map((item, index) => {
+      return <p key={index}>{item.name}</p>;
+    })
+  ) : (
+    <>loading.......</>
+  );
+}
 
-  return state.map((item) => {
-    return <p>{item.name}</p>;
-  });
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const data = await response.json();
+
+  return { props: { state: data }, revalidate: 10 };
 }
 
 export default LastSalesPage;
