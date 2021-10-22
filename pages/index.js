0a3eec8176dd32a5d0386fe118671fc77a335e22
Giimhana Dissanayake <giimhanadissanayake@gmail.com>
2021-10-22 13:40:47 +0530

    utilizing incremental static generation(ISR)

--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,15 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
+  console.log("(Re-)Generating...");
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
-
   const data = JSON.parse(jsonData);
 
   return {
     props: {
       products: data.products,
     },
+    revalidate: 10,
   };
 }
