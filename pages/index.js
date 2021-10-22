ec57074c94bf42b39cef70cbd0f409f67bf11e8b
Giimhana Dissanayake <giimhanadissanayake@gmail.com>
2021-10-22 13:29:49 +0530

    90.running server-side code and using the file system

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,28 @@
 import styles from "../styles/Home.module.css";
+import path from "path";
+import fs from "fs/promises";
 
-export default function Home() {
+export default function Home(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+
+  const data = JSON.parse(jsonData);
+
+  return {
+    props: {
+      products: data.products,
+    },
+  };
 }
