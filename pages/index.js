e9e6326511c1dc901d66c1dfe93b91421c961066
Giimhana Dissanayake <giimhanadissanayake@gmail.com>
2021-10-22 15:23:56 +0530

    95.working with dynamic parameters

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,17 @@ import styles from "../styles/Home.module.css";
 import path from "path";
 import fs from "fs/promises";
 
+import Link from "next/link";
+
 export default function Home(props) {
   const { products } = props;
 
   return (
     <ul>
       {products.map((product) => (
-        <li key={product.id}>{product.title}</li>
+        <li key={product.id}>
+          <Link href={`/${product.id}`}>{product.title}</Link>
+        </li>
       ))}
     </ul>
   );
